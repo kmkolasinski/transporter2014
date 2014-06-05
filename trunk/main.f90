5882d9d419604d9040b0ac3af9339536fc58af19
KMKolasinski@gmail.com <KMKolasinski@gmail.com@de6807f4-dad3-2799-43e2-ac09c0ee6d87>
2014-06-05 13:04:44 +0000

    nowe zmiany ciagle jednopasmowo

--- trunk/main.f90
+++ trunk/main.f90
@@ -18,8 +18,10 @@ program transporter
  double precision,parameter :: dx   = 2;
  integer :: zwidth
  double precision, dimension(:,:),allocatable :: Utmp
- integer :: i,j,riter,niter
+ integer :: i,j,riter,niter,giter,iter
  double precision :: T_SUM
+ doubleprecision :: xp,yp,x0,y0,amp,sigma
+ character(len=11) :: plik
 ! -----------------------------------------------
 ! Zmienne wczytywane z config.ini
 ! -----------------------------------------------
@@ -37,6 +39,26 @@ call system_inicjalizacja(NX,NY,liczba_zrodel,DX);
 
 
 zwidth = 15
+iter = 1
+do atomic_Bz = 0.0,0.10,0.01
+call zrodla(1)%zrodlo_ustaw(1,NY,1,dx,atomic_Ef,-atomic_Bz,ZRODLO_KIERUNEK_PRAWO,2,UTOTAL)
+
+if(iter < 10) then
+    write(plik(1:1),"(i1)"),iter
+    plik(2:9) = "-ave.txt"
+else if(iter < 100) then
+    write(plik(1:2),"(i2)"),iter
+    plik(3:10) = "-ave.txt"
+else if(iter < 1000) then
+    write(plik(1:3),"(i3)"),iter
+    plik(4:11) = "-ave.txt"
+endif
+print*,"dane/"//plik
+call zrodla(1)%zrodlo_zapisz_mody("dane/"//plik,dx)
+
+iter = iter +  1
+enddo
+stop
 call zrodla(1)%zrodlo_ustaw(NY/2-zwidth,NY/2+zwidth,1,dx,atomic_Ef,atomic_Bz,ZRODLO_KIERUNEK_PRAWO,2,UTOTAL)
 call zrodla(2)%zrodlo_ustaw(NY/2-zwidth,NY/2+zwidth,NX,dx,atomic_Ef,atomic_Bz,ZRODLO_KIERUNEK_LEWO,2,UTOTAL)
 call utworz_system()
@@ -49,21 +71,31 @@ allocate(Utmp(nx,ny))
 Utmp = UTOTAL
 
 
-open(unit = 111, file= "TodIter.txt" )
+open(unit = 111, file= "TodIter4.txt" )
 !do atomic_Bz = 0.0,0.3,0.002
-do atomic_Ef = 2.0,6.0,0.05
-niter = 1
+do atomic_Bz = 0.2,0.4,0.002
+niter = 50
 T_SUM = 0
 riter = 1
-!do riter = 1 , niter
+do riter = 1 , niter
 
-UTOTAL = Utmp
-do i = 100 , nx-100
-do j = 1 , ny
-    UTOTAL(i,j) = UTOTAL(i,j) + 2*(rand() - 0.5)*atomic_Ef/1000.0/Rd/3000.0
-end do
-end do
 
+
+UTOTAL = 0
+do giter  = 1, 5
+    x0  = rand()*(nx-100)+50
+    y0  = rand()*(ny)
+    amp   = -0.5*atomic_Ef/1000.0/Rd
+    sigma = 0.1
+
+    do i = 50 , nx-50
+    do j = 1 , ny
+        xp = i
+        yp = j
+       UTOTAL(i,j) = UTOTAL(i,j) + system_gauss(xp,yp,x0,y0,sigma,amp)
+    end do
+    end do
+enddo
 
 call zrodla(1)%zrodlo_ustaw(NY/2-zwidth,NY/2+zwidth,1,dx,atomic_Ef,atomic_Bz,ZRODLO_KIERUNEK_PRAWO,2,UTOTAL)
 !call zrodla(1)%zrodlo_zapisz_mody("mody1.txt",dx)
@@ -71,7 +103,8 @@ call zrodla(2)%zrodlo_ustaw(NY/2-zwidth,NY/2+zwidth,NX,dx,atomic_Ef,atomic_Bz,ZR
 !call zrodla(2)%zrodlo_zapisz_mody("mody2.txt",dx)
 call system_rozwiaz_problem(1)
 T_SUM = T_SUM + TRANS_T
-write(111,"(i10,4e20.8)"),riter,atomic_Ef,T_SUM/riter,TRANS_T+TRANS_R
+enddo
+write(111,"(i10,4e20.8)"),riter,atomic_Bz,T_SUM/niter,TRANS_T+TRANS_R
 enddo
 
 close(111)
@@ -81,6 +114,7 @@ close(111)
 
 call system_zapisz_do_pliku("flagi.txt",ZAPISZ_FLAGI);
 call system_zapisz_do_pliku("phi.txt",ZAPISZ_PHI);
+call system_zapisz_do_pliku("utotal.txt",ZAPISZ_POTENCJAL);
 call system_zwalnienie_pamieci()
 deallocate(Utmp)
 
@@ -89,19 +123,19 @@ contains
 subroutine utworz_system()
     integer :: i,j , wjazd , promien
     ! prosty test
-    wjazd = 210
+    wjazd = 110
     !GFLAGS(200+NX/2-20:200+NX/2,1:NY) = B_EMPTY
 
-!    do i = 1 , nx
-!    do j = 1 , ny
-!        promien = sqrt((i - nx/2.0)**2 + (j - ny/2.0)**2)
-!        if( promien > ny/2 .or. promien < ny/2-20  ) then
-!             GFLAGS(i,j) = B_EMPTY
-!        endif
-!    enddo
-!    enddo
-    UTOTAL(NX/2-10-50:NX/2+10-50,:) = atomic_Ef/1000.0/Rd/5
-    UTOTAL(NX/2-10+50:NX/2+10+50,:) = atomic_Ef/1000.0/Rd/5
+    do i = 1 , nx
+    do j = 1 , ny
+        promien = sqrt((i - nx/2.0)**2 + (j - ny/2.0)**2)
+        if( promien > ny/2 .or. promien < ny/2-40  ) then
+             GFLAGS(i,j) = B_EMPTY
+        endif
+    enddo
+    enddo
+    !UTOTAL(NX/2-150:NX/2+150,:) = -atomic_Ef/1000.0/Rd*5
+    !UTOTAL(NX/2-10+50:NX/2+10+50,:) = atomic_Ef/1000.0/Rd/5
 
     GFLAGS(1:wjazd,:)                 = B_EMPTY
     GFLAGS(NX-wjazd:NX,:)             = B_EMPTY
