f6d1e4059666a76576ae804930da12b01e7ce354
KMKolasinski@gmail.com <KMKolasinski@gmail.com@de6807f4-dad3-2799-43e2-ac09c0ee6d87>
2014-08-14 12:03:18 +0000

    nowy sposob liczenia modow poprzecznych, znajduje od razu k. Jeszcze w powijakach

--- trunk/main.f90
+++ trunk/main.f90
@@ -38,30 +38,32 @@ call modjed_ustaw_konwersje_jednostek(M_EFF,E_MAT);
 call system_inicjalizacja(NX,NY,liczba_zrodel,DX);
 
 
-zwidth = 15
+zwidth = 19
 iter = 1
-do atomic_Bz = 0.0,0.10,0.01
-call zrodla(1)%zrodlo_ustaw(1,NY,1,dx,atomic_Ef,-atomic_Bz,ZRODLO_KIERUNEK_PRAWO,2,UTOTAL)
-
-if(iter < 10) then
-    write(plik(1:1),"(i1)"),iter
-    plik(2:9) = "-ave.txt"
-else if(iter < 100) then
-    write(plik(1:2),"(i2)"),iter
-    plik(3:10) = "-ave.txt"
-else if(iter < 1000) then
-    write(plik(1:3),"(i3)"),iter
-    plik(4:11) = "-ave.txt"
-endif
-print*,"dane/"//plik
-call zrodla(1)%zrodlo_zapisz_mody("dane/"//plik,dx)
-
-iter = iter +  1
-enddo
-stop
+!do atomic_Bz = 0.0,0.10,0.01
+!call zrodla(1)%zrodlo_ustaw(1,NY,1,dx,atomic_Ef,-atomic_Bz,ZRODLO_KIERUNEK_PRAWO,2,UTOTAL)
+!
+!if(iter < 10) then
+!    write(plik(1:1),"(i1)"),iter
+!    plik(2:9) = "-ave.txt"
+!else if(iter < 100) then
+!    write(plik(1:2),"(i2)"),iter
+!    plik(3:10) = "-ave.txt"
+!else if(iter < 1000) then
+!    write(plik(1:3),"(i3)"),iter
+!    plik(4:11) = "-ave.txt"
+!endif
+!print*,"dane/"//plik
+!call zrodla(1)%zrodlo_zapisz_mody("dane/"//plik,dx)
+!
+!iter = iter +  1
+!enddo
+!stop
 call zrodla(1)%zrodlo_ustaw(NY/2-zwidth,NY/2+zwidth,1,dx,atomic_Ef,atomic_Bz,ZRODLO_KIERUNEK_PRAWO,2,UTOTAL)
+
 call zrodla(2)%zrodlo_ustaw(NY/2-zwidth,NY/2+zwidth,NX,dx,atomic_Ef,atomic_Bz,ZRODLO_KIERUNEK_LEWO,2,UTOTAL)
 call utworz_system()
+stop
 call system_zapisz_do_pliku("flagi.txt",ZAPISZ_FLAGI);
 
 ! -------------------------------------------------------------------
